c4ea68f1efabe0e79df6b5cca65578a78816c9fc
loukim86 <148028680+loukim86@users.noreply.github.com>
2024-01-22 16:35:20 +0900

    Update NewTaskForm.js

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -1,41 +1,14 @@
-import React, { useState } from 'react';
-import { formatDistanceToNow } from 'date-fns';
+import React from 'react';
 import './NewTaskForm.css';
 
 
-const NewTaskForm = ({ onAddTask }) => {
-  const [newTask, setNewTask] = useState('');
-
-  const handleInputChange = (event) => {
-    setNewTask(event.target.value);
-  }; 
-
-const handleAddTask = () => {
-    if (newTask.trim() !== '') {
-      const taskDate = new Date(); 
-      const formattedTime = getTimeAgo(taskDate);
-  
-      onAddTask({ description: newTask, status: '', created: formattedTime });
-      setNewTask('');
-    }
-  };
-  
-
-const getTimeAgo = (taskDate) => {
-    return formatDistanceToNow(taskDate, { addSuffix: true });
-  };
-  
-
-  return (
-    <input
-      className="new-todo"
-      placeholder="What needs to be done?"
-      autoFocus
-      value={newTask}
-      onChange={handleInputChange}
-      onKeyDown={(e) => e.key === 'Enter' && handleAddTask()}
-    />
-  );
+const NewTaskForm = () => {
+    return (
+    <input className="new-todo"
+           placeholder="What needs to be done?"
+           autoFocus/>
+     );
 };
+
 
 export default NewTaskForm;
