4701510fe325c0e65a566c7e2f729e617d5bf5cb
loukim86 <148028680+loukim86@users.noreply.github.com>
2024-01-22 16:36:15 +0900

    Update TasksFilter.js

--- src/components/TasksFilter/TasksFilter.js
+++ src/components/TasksFilter/TasksFilter.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import './TasksFilter.css';
+import './TasksFilter.css'
 
-const TasksFilter = ({ onFilterChange }) => {
-  return (
-    <ul className="filters">
-      <li>
-        <button className="selected" onClick={() => onFilterChange('all')}>
-          All
-        </button>
-      </li>
-      <li>
-        <button onClick={() => onFilterChange('active')}>Active</button>
-      </li>
-      <li>
-        <button onClick={() => onFilterChange('completed')}>Completed</button>
-      </li>
-    </ul>
-  );
-};
+
+const TasksFilter = () => {
+    return (
+        <ul className="filters">
+            <li>
+                <button className="selected">All</button>
+            </li>
+            <li>
+                <button>Active</button>
+            </li>
+            <li>
+                <button>Completed</button>
+            </li>
+        </ul>
+    );
+}
 
 export default TasksFilter;
