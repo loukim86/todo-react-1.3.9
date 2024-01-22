74f3837bcd386f44a8cd448c30c3b2aea3d598fe
loukim86 <148028680+loukim86@users.noreply.github.com>
2024-01-22 16:34:43 +0900

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,14 @@ import React from 'react';
 import NewTaskForm from "../NewTaskForm/NewTaskForm";
 import './Header.css';
 
-const Header = ({ onAddTask, newTask, setNewTask }) => {
-  return (
-    <header className="header">
-      <h1>todos</h1>
-      <NewTaskForm onAddTask={onAddTask}
-        newTask={newTask}
-        setNewTask={setNewTask} />
-    </header>
-  );
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>todos</h1>
+            <NewTaskForm/>
+        </header>
+    );
 }
+
 
 export default Header;
